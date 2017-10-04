74ca235c82670e79d8a67be8bec9fe5275ff077a
Stefan Andrei <picotto@Stefans-MacBook-Pro-2.local>
2017-10-04 14:31:17 +0200

    js2

--- blah2/js/javascript.js
+++ blah2/js/javascript.js
@@ -2,5 +2,5 @@ let anim = document.querySelector ("#anim");
 anim.addEventListener('click', startAnimation);
 
 function startAnimation() {
-    anim.classList.add('enable');
+    anim.classList.toggle('enable');
 }
